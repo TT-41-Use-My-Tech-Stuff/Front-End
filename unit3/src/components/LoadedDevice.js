8483d9fa6a65a2460af6c3da6aff25ce3b08270b
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-10-17 16:06:38 -0700

    late initial push; have worked on most of the components

---
+++ unit3/src/components/LoadedDevice.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import DeviceCard from './DeviceCard'
+const Devices = ({ devices, getDeviceByID }) => {
+  if (devices === undefined) {
+    return <div> Out of stock </div>;
+  }
+  return (
+    <div className="device-container">
+      {devices.map((device, index) => {
+        return <DeviceCard key={index} device={device} getDeviceByID={getDeviceByID} />;
+      })}
+    </div>
+  );
+};
+
+export default Devices;
---
+++ unit3/src/utils/axiosWithAuth.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+  // Set token in local storage
+  const token = localStorage.getItem("token");
+  return axios.create({
+    //pass token through header
+    headers: {
+      Authorization: token,
+    },
+    //set base url for easier requests
+    baseURL: "https://waterplantsunit4.herokuapp.com",
+  });
+};
